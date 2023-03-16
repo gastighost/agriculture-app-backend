84fdafb985286cc60757ae7843fc90f5f1b15815
Gaston Roxas <gacaro51597@gmail.com>
2023-03-16 16:32:08 +0800

    add cors and helmet dependency

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+
 import { AppModule } from './app.module';
 import { PrismaClientExceptionFilter } from './common/filters/prisma-client-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+
+  app.enableCors({ origin: ['http://localhost:3000'] });
 
   app.useGlobalPipes(
     new ValidationPipe({
