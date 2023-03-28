da813af30bfe4a8065bb6d21785b98967a048af8
Gaston Roxas <gacaro51597@gmail.com>
2023-03-28 11:07:07 +0800

    add cron dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
+
 import { PrismaModule } from './services/prisma/prisma.module';
 import { LocationsModule } from './models/locations/locations.module';
 import { FarmsModule } from './models/farms/farms.module';
 import { UsersModule } from './models/users/users.module';
 import { AuthModule } from './services/auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
 import { CropsModule } from './models/crops/crops.module';
 import { MarketsModule } from './models/markets/markets.module';
 import { WebsocketsModule } from './services/websockets/websockets.module';
+import { TasksModule } from './services/tasks/tasks.module';
 
 @Module({
   imports: [
@@ -22,6 +25,8 @@ import { WebsocketsModule } from './services/websockets/websockets.module';
     CropsModule,
     MarketsModule,
     WebsocketsModule,
+    ScheduleModule.forRoot(),
+    TasksModule,
   ],
 })
 export class AppModule {}
